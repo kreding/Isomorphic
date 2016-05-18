820f6dd71fcb0e6e38ab77ba182ee250965ae966
鞠学健 <juxuejian@meituan.com>
2016-05-18 16:08:23 +0800

    perfect bg1

--- app/components/Me.jsx
+++ app/components/Me.jsx
@@ -32,8 +32,6 @@ export default class Me extends React.Component {
       videoEle.style.width = "auto";
       videoEle.style.height = window.innerHeight + "px";
     }
-
-    headerEle.style.display = "block";
   }
 
   render() {
